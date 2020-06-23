d410ea0c1d407967d1c74a1feba7dfabdb22b24b
Rafa1104 <58662403+Rafa1104@users.noreply.github.com>
2020-06-23 11:52:58 -0500

    Update AporteRPI.py

--- Aportes/Raspberry/AporteRPI.py
+++ Aportes/Raspberry/AporteRPI.py
@@ -1,4 +1,8 @@
+##Correr este script despues de ejecutar en terminal lo siguiente:
+##sudo socat -d -d -d -v -x PTY,link=/dev/ttyUSB10,mode=777,unlink-close,raw,echo=0 PTY,link=/dev/ttyUSB20,mode=777,unlink-close,raw,echo=0
 
+##para leer la señal desde el puerto
+##cat < /dev/ttyUSB10
 
 import serial, time
 from random import randrange, uniform
